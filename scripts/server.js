b2bbdf3b091a7793bd0d0a933262f14e2c834aed
alikahwaji <ali.kahwaji@nmit.ac.nz>
2020-09-13 18:01:15 +1200

    Added a routing request

--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url
+
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter a Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</head>')
+        res.write('</html>')
+        return res.end()
+    }
     // process.exit()
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
-    res.write('<head><title>My Node page</title></head>')
-    res.write('<body><h1>Hello from my Node server</h1></body>')
-    res.write('</head>')
+    res.write('<head><title>My Node Page</title></head>')
+    res.write('<body><h1> Hello from My Node Server </h1></body>')
+    res.write('</html>')
     res.end()
 });
 
